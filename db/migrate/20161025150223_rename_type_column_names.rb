011448c3411793ba3d34a72c5a7579a5a676a845
Laura Wilson <laurawilson@hotmail.co.uk>
2016-10-25 16:39:05 +0100

    closes 3 - homepage has link to new test

--- db/migrate/20161025150223_rename_type_column_names.rb
+++ db/migrate/20161025150223_rename_type_column_names.rb
@@ -1,6 +1,6 @@
 class RenameTypeColumnNames < ActiveRecord::Migration[5.0]
   def change
-    rename_column :default_questions, :type, :format
-    rename_column :answers, :type, :format 
+    rename_column :questions, :type, :format
+    rename_column :answers, :type, :format
   end
 end
