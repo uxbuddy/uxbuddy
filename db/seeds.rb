ba01013e51d82215efcd92f50c53a2ae461ca435
Matthew Perkins <mfperkins@gmail.com>
2016-10-26 16:02:40 +0100

    adding all relevant tests for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Starting to seed db..."
-
 
 Rake::Task['load_data:add_questions'].invoke
 Rake::Task['load_data:add_test_types'].invoke
@@ -15,6 +9,16 @@ Rake::Task['load_data:add_test_types'].invoke
 Test.create(name: 'Youtube', test_url: "https://www.youtube.com/embed/XGSy3_Czz8k", test_type_id: 1)
 Test.create(name: 'Asos', test_url: "https://www.asos.com", test_type_id: 1)
 Test.create(name: 'Google', test_url: "https://www.google.com", test_type_id: 1)
+
+Test.all.each do |test|
+  test.questions << Question.where(id: 1)
+  test.questions << Question.where(id: 2)
+  test.questions << Question.where(id: 3)
+end
+
+Question.all.each do |question|
+  question.test_types << TestType.where(id: 1)
+end
 
 5.times do
   Answer.create(format: "Range", response: 4, question_id: 1)
--- spec/test_type_spec.rb
+++ spec/test_type_spec.rb
@@ -2,8 +2,14 @@ describe TestType do
 
   describe "Load data" do
 
+    let(:product_type) { TestType.find(1) }
+
     it 'should load some data at the start' do
       expect(TestType.count).to eq(1)
+    end
+
+    it 'should have 8 associated questions' do
+      expect(product_type.questions.count).to eq(8)
     end
 
   end
