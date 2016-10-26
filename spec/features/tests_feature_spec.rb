33d1e244e229b162b13ff29088cecd4f94173239
Rosie Allott <rosie.allott@gmail.com>
2016-10-26 16:14:21 +0100

    Testing hiding showing of divs

--- spec/features/tests_feature_spec.rb
+++ spec/features/tests_feature_spec.rb
@@ -9,6 +9,19 @@ feature 'Tests' do
       visit "/tests/show"
       expect(page).to have_content 'Welcome'
       expect(page).to have_xpath("//iframe[@src= 'https://www.youtube.com/embed/XGSy3_Czz8k']")
+      expect(page).not_to have_xpath("//iframe[@src= 'other-url']")
     end
+  end
+
+  context "questions rendered" do
+
+    let!(:test) { Test.create(name: 'Youtube', test_url: "https://www.youtube.com/embed/XGSy3_Czz8k") }
+
+    # scenario 'user cannot initially see questions' do
+    #   visit "/tests/show"
+    #   expect(page).not_to have_content("Question 1")
+    #   click_link('Next')
+    #   expect(page).to have_content("Question 1")
+    # end
   end
 end
