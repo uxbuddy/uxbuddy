414a644fd3e148492cc9eb25c2c2f46d2682fd19
Laura Wilson <laurawilson@hotmail.co.uk>
2016-10-26 13:01:37 +0100

    questions showing in form

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,13 @@ class TestsController < ApplicationController
 
   def new
     @test = Test.new
+    @questions = Question.all
+    @question1 = Question.find(1)
+    @question2 = Question.find(2)
   end
+
+  def create
+  end 
 
   def show
     @test = Test.find(params[:id])
