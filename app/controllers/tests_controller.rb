3a7767702160cb1b3ac7fd28c0175054819c880f
Laura Wilson <laurawilson@hotmail.co.uk>
2016-10-26 14:31:07 +0100

    test written for creating tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,13 +2,17 @@ class TestsController < ApplicationController
 
   def new
     @test = Test.new
+    @test_types = TestType.first
     @questions = Question.all
     @question1 = Question.find(1)
     @question2 = Question.find(2)
   end
 
   def create
-  end 
+    @test = Test.new(name: params[:name], test_url: params[:test_url], test_type_id: params[:test_type])
+    @test.questions << Question.find(1)
+    @test.save
+  end
 
   def show
     @test = Test.find(params[:id])
