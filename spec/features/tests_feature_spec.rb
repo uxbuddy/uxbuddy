6df68cb54fd9a836bd6bae7e9cb7eb51ef754daf
Rosie Allott <rosie.allott@gmail.com>
2016-10-26 16:30:20 +0100

    Hiding and showing start and next buttons

--- spec/features/tests_feature_spec.rb
+++ spec/features/tests_feature_spec.rb
@@ -5,11 +5,17 @@ feature 'Tests' do
 
     let!(:test) { Test.create(name: 'Youtube', test_url: "https://www.youtube.com/embed/XGSy3_Czz8k") }
 
-    scenario 'user sees iframe url when starting test' do
-      visit "/tests/show"
+    before do
+      visit "/tests/show" #need to change to actual test url
+    end
+    scenario 'user sees welcome page when they go to test url' do
       expect(page).to have_content 'Welcome'
+    end
+    scenario 'user sees iframe and question 1 when click Start button' do
+      click_button('Start')
       expect(page).to have_xpath("//iframe[@src= 'https://www.youtube.com/embed/XGSy3_Czz8k']")
       expect(page).not_to have_xpath("//iframe[@src= 'other-url']")
+      expect(page).to have_content("Question 1")
     end
   end
 
@@ -17,11 +23,11 @@ feature 'Tests' do
 
     let!(:test) { Test.create(name: 'Youtube', test_url: "https://www.youtube.com/embed/XGSy3_Czz8k") }
 
-    # scenario 'user cannot initially see questions' do
-    #   visit "/tests/show"
-    #   expect(page).not_to have_content("Question 1")
-    #   click_link('Next')
-    #   expect(page).to have_content("Question 1")
-    # end
+    scenario 'user cannot initially see questions' do
+      visit "/tests/show"
+      expect(page).not_to have_content("Question 2")
+      click_link('Next')
+      expect(page).to have_content("Question 2")
+    end
   end
 end
