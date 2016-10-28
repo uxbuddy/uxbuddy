03bc0d18692925e2cc4e9a3602f276f60592bf3c
mrmurtz <murtaza.s.abidi@gmail.com>
2016-10-28 11:19:33 +0100

    Summary info displays, tests passing

--- spec/features/report_feature_spec.rb
+++ spec/features/report_feature_spec.rb
@@ -6,18 +6,13 @@ feature 'report page' do
     expect(page.status_code).to equal(200)
   end
 
-  scenario 'report page shows answers to first question' do
-    visit '/tests/1/report'
-    expect(page.all('table#q1-results tr').count).to eq(5)
-  end
-
   scenario 'report page has test name in header' do
     visit '/tests/1/report'
+    expect(page).to have_content("Youtube // Test Report")
     within '#summary' do
-      expect(page).to have_content("Youtube // Test Report")
       expect(page).to have_content("Test URL: https://www.youtube.com/embed/XGSy3_Czz8k")
+      expect(page).to have_content("Number of questions: 3")
       expect(page).to have_content("Number of respondents: 5")
-      expect(page).to have_content("Number of questons: 5")
     end
   end
 end
