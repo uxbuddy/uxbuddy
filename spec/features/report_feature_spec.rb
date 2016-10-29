4671b59ff1916a73eeeb6f62e48e0d15c455ad69
Rosie Allott <rosie.allott@gmail.com>
2016-10-29 16:04:02 +0100

    DB reseeded and tests passing

--- spec/features/report_feature_spec.rb
+++ spec/features/report_feature_spec.rb
@@ -6,13 +6,13 @@ feature 'report page' do
     expect(page.status_code).to equal(200)
   end
 
-  scenario 'RANDOM SEEDING OF TEST FOR ANSWER - report page has test name in header' do
+  scenario 'report page has test name in header' do
     visit '/tests/1/report'
     expect(page).to have_content("Youtube // Test Report")
     within '#summary' do
       expect(page).to have_content("Test URL: https://www.youtube.com/embed/XGSy3_Czz8k")
       expect(page).to have_content("Number of questions: 3")
-      # expect(page).to have_content("Number of respondents: 5")
+      expect(page).to have_content("Number of respondents: 3")
     end
   end
 end
