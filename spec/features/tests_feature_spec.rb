7978c2720a823a9153f37f0492db84c9f0e98f24
Matthew Perkins <mfperkins@gmail.com>
2016-10-29 18:35:21 +0100

    refactor tests to get everything working

--- spec/features/tests_feature_spec.rb
+++ spec/features/tests_feature_spec.rb
@@ -46,7 +46,7 @@ feature 'User can see a test' do
     scenario 'User can submit answers', js: true do
       visit "/tests/youtube"
       click_button('Start')
-      find(:xpath, "//input[@id='range1']").set 2
+      find(:xpath, "//input[@id='range1']").set 1
       click_link('Next')
       expect{click_link('Next')}.to change(Answer, :count).by(1)
     end
