58c8ecf5621b35463bedc5526509efb6c3ff9e70
Rosie Allott <rosie.allott@gmail.com>
2016-10-29 13:40:12 +0100

    Closes #87 by sorting out DB assoc answers -> test

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,7 +1,7 @@
 class ReportController < ApplicationController
 
   def index
-    @test = Test.find(params[:test_id])
-    @answers = @test.answers
+    @test = Test.friendly.find(params[:test_id])
+    @respondents = @test.questions[0].answers.where(test_id: @test.id).count
   end
 end
