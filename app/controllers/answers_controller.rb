2f868d5471fdf6552db543513e38f1dc830b1a5f
Laura Wilson <laurawilson@hotmail.co.uk>
2016-10-30 12:33:17 +0000

    correct question id saving to answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,14 +2,23 @@ class AnswersController < ApplicationController
 
   def create
     answer = Answer.new(answer_params)
+    @test = Test.friendly.find(params[:test_id])
+    answer.test_id = @test.id
+    @questions = @test.questions
+    puts "HIIIIIII"
+    puts @test.id
+    puts @questions[0]
+    puts @questions[1]
+    p params[:question_no].to_i
+    puts @questions[params[:question_no].to_i]
+    answer.question_id = @test.questions[params[:question_no].to_i].id
     answer.format = answer.question.format
-    answer.test_id = Test.friendly.find(params[:test_id]).id
     answer.save
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:format, :response, :question_id, :test_id)
+    params.require(:answer).permit(:format, :response, :question_no, :question_id, :test_id)
   end
 end
