8fb159de01a2dfee67bf0dbc483e4c38ae1cb4b2
Rosie Allott <rosie.allott@gmail.com>
2016-10-30 16:36:17 +0000

    88-question-buttons fixing tests

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -10,11 +10,11 @@ describe Test do
       expect(test3.questions.count).to eq(3)
     end
 
-    xit 'Test 1 should have 9 answers' do # skipped as changes when you add more tests for answers
+    it 'Test 1 should have 9 answers' do # skipped as changes when you add more tests for answers
       expect(test1.answers.where(test_id: test1.id).count).to eq(9)
     end
 
-    xit 'Test 1 should have 3 answers to Question 1' do # skipped as changes when you add more tests for answers 
+    it 'Test 1 should have 3 answers to Question 1' do # skipped as changes when you add more tests for answers 
       question_1 = test1.questions[0]
       answers_to_question_1 = question_1.answers.where(test_id: test1.id)
       expect(answers_to_question_1.count).to eq(3)
