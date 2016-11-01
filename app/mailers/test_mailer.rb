37fb92845dd2a6ff2980a5fceb13b7e1197b9c83
Laura Wilson <laurawilson@hotmail.co.uk>
2016-11-01 22:31:35 +0000

    adds 'from: user' attribute to email

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,9 +1,10 @@
 class TestMailer < ApplicationMailer
-  default :from => 'no-reply@uxbuddy.herokuapp.com'
+  # default :from => 'no-reply@uxbuddy.herokuapp.com'
 
   def send_test_email(user, list, url)
     @url = url
     mail(to: list,
+    from: user.email,
     subject: 'Please take our UX test!' )
   end
 
