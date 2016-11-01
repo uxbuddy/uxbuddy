7b14b3682ef47c58985969d6468859c8550fc9c9
Laura Wilson <laurawilson@hotmail.co.uk>
2016-11-01 11:19:45 +0000

    adds url as argument to mailer method

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,7 +1,8 @@
 class TestMailer < ApplicationMailer
   default from: 'user@uxbuddy.com'
 
-  def send_test_email(list)
+  def send_test_email(list, url)
+    @url = url
     mail( to: list,
     from: current_user.email,
     subject: 'Please take our UX test!' )
