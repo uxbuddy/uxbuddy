badc47bfdb5c898e6b1f06b1edda260b05cc5a55
Matthew Perkins <mfperkins@gmail.com>
2016-11-01 16:50:42 +0000

    refactored tests

--- spec/features/report_feature_spec.rb
+++ spec/features/report_feature_spec.rb
@@ -35,9 +35,10 @@ feature 'report page' do
     end
   end
 
-  xscenario 'average chart does not display if no reponses' do
+  scenario 'average chart does not display if no reponses' do
     user_sign_in
-    visit '/reports/1'
+    create_test("BBC", "http://www.bbc.co.uk")
+    visit '/reports/5'
     within '#report-chart' do
       expect(page.body).to have_content("Sorry, no data yet!")
     end
