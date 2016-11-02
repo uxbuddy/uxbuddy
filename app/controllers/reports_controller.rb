26800246cb48945a8b369a25a6976cb72c19256d
Rosie Allott <rosie.allott@gmail.com>
2016-11-02 12:21:50 +0000

    Fix bug so that users can only see their own reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,15 +2,15 @@ class ReportsController < ApplicationController
 
   def show
     if user_signed_in?
-    @test = Test.friendly.find(params[:id])
-    @questions = @test.questions
-    @respondents = @test.number_respondents
-    @library = {xtitle: 'Average response',
-                hAxis: {ticks: [1,2,3,4,5]},
-                chartArea: {left: 500, width: 600},
-                }
-    answers = @test.answers
-    @data = answers.where(test_id: @test.id).group('text').average(:response)
+      @test = Test.friendly.find(params[:id])
+      @questions = @test.questions
+      @respondents = @test.number_respondents
+      @library = {xtitle: 'Average response',
+                  hAxis: {ticks: [1,2,3,4,5]},
+                  chartArea: {left: 500, width: 600},
+                  }
+      answers = @test.answers
+      @data = answers.where(test_id: @test.id).group('text').average(:response)
     else
       flash[:notice] = 'Please sign in to view your reports'
       redirect_to new_user_registration_path
