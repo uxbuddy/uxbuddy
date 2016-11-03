725cc47fbf29f30066a737c327bbd8898be225c6
Matthew Perkins <mfperkins@gmail.com>
2016-11-03 10:23:44 +0000

    continue refactor

--- spec/features/report_feature_spec.rb
+++ spec/features/report_feature_spec.rb
@@ -2,51 +2,49 @@ require 'rails_helper'
 
 feature 'report page' do
 
-  let!(:test) { Test.find(1) }
-  let!(:question1) {test.questions[0]}
-  let!(:answers) {question1.answers.where(test_id: test.id)}
-  let!(:response1) {answers[0].response}
-  let!(:response2) {answers[1].response}
-  let!(:response3) {answers[2].response}
-  let!(:average) {(response1 + response2 + response3)/3.0}
+  before(:each) do
+    Test.create(name: 'bbc', test_url: "https://www.bbc.co.uk/news", test_type_id: 1, slug: "bbc", question_ids: [1,4,])
+    Test.create(name: 'github', test_url: "https://github.com/lili2311/trelogan-yoga/blob/master/src/about.html", test_type_id: 1, slug: "github", question_ids: [1])
+  end
+
+  # let!(:test) { Test.find(1) }
+  # let!(:question1) {test.questions[0]}
+  # let!(:answers) {question1.answers.where(test_id: test.id)}
+  # let!(:response1) {answers[0].response}
+  # let!(:response2) {answers[1].response}
+  # let!(:response3) {answers[2].response}
+  # let!(:average) {(response1 + response2 + response3)/3.0}
 
   scenario 'report page has right test info' do
     user_sign_in
-    visit '/reports/1'
-    expect(page).to have_content("Youtube // Test Report")
+    visit '/reports/bbc'
+    expect(page).to have_content("BBC // Test Report")
     within '#summary' do
       expect(page).to have_content("Test URL: https://www.youtube.com/embed/XGSy3_Czz8k")
       expect(page).to have_content("Number of questions: 3")
       expect(page).to have_content("Number of respondents: 3")
       expect(page).to have_content("Percentage completion: 100%")
-      within '#report-chart' do
-        expect(page.body).to have_content("How easy would you find it to buy these?")
     end
-  end
-
-  scenario 'report page has an average chart' do
-    user_sign_in
-    visit '/reports/1'
-
+    within '#report-chart' do
+      expect(page.body).to have_content("How easy would you find it to buy these?")
+    end
+    within '#question1-comments' do
+      expect(page).to have_content('wow')
     end
   end
 
   scenario 'average chart does not display if no reponses' do
     user_sign_in
     create_test("BBC", "http://www.bbc.co.uk")
-    visit '/reports/5'
+    visit '/reports/bbc'
     within '#report-chart' do
       expect(page.body).to have_content("Sorry, no data yet!")
     end
   end
 
-
-  scenario 'report page displays comments for each question' do
-    user_sign_in
+  scenario 'User can only view reports page when signed in' do
     visit '/reports/1'
-    within '#question1-comments' do
-      expect(page).to have_content('wow')
-    end
+    expect(page).to have_current_path('/users/sign_up')
   end
 
   scenario 'Nav bar chart icon links to tests#index' do
@@ -56,11 +54,6 @@ feature 'report page' do
       find(:xpath, "//a[@id='tests-all-1']").click
       expect(page).to have_current_path("/tests")
     end
-  end
-
-  scenario 'User can only view reports page when signed in' do
-    visit '/reports/1'
-    expect(page).to have_current_path('/users/sign_up')
   end
 
   scenario 'reports index shows list of user test reports' do
--- spec/features/tests_feature_spec.rb
+++ spec/features/tests_feature_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 feature 'User can see a test' do
 
   before(:each) do
-    Test.create(name: 'youtube', test_url: "https://www.youtube.com/watch?v=Fz4F2X1xSc8", test_type_id: 1, slug: "youtube", question_ids: [1])
+    Test.create(name: 'youtube', test_url: "https://www.youtube.com/watch?v=Fz4F2X1xSc8", test_type_id: 1, slug: "youtube", question_ids: [1,3,5])
     Test.create(name: 'github', test_url: "https://github.com/lili2311/trelogan-yoga/blob/master/src/about.html", test_type_id: 1, slug: "github", question_ids: [1])
   end
 
   context "Github file tests" do
 
     scenario 'iframe has correct source for github urls', js: true do
-      tests = Test.all
-      tests.each { |x| puts x.id, x.slug}
       visit "/tests/github"
       click_on('Start')
       expect(page).to have_xpath("//iframe[@src= 'https://htmlpreview.github.io/?https://github.com/lili2311/trelogan-yoga/blob/master/src/about.html']")
@@ -22,20 +20,13 @@ feature 'User can see a test' do
   context "User can complete a test" do
 
     scenario 'User can submit answers', js: true do
-      tests = Test.all
-      tests.each { |x| puts x.id, x.slug}
       visit "/tests/youtube"
       click_on('Start')
-      save_and_open_page
-      puts page.body
-      # within '.range-label' do
-      #   find(:xpath, "//span[text()]").set 1
-      # end
       fill_in 'comment1', with: 'This is so boss'
       click_link('Next')
       click_link('Next')
       click_link('Finish')
-      expect{click_link('Next')}.to change(Answer, :count).by(1)
+      expect(Answer.count).to eq(3)
       expect(page).to have_current_path("/tests/youtube/thanks")
       message = "Thank you for completing this test."
       expect(page).to have_content(message)
